1f92d3b52904edc89ae8e7dc4fc6990db63af89c
zhaoyao91 <zhaoyao91@163.com>
2017-08-19 21:43:27 +0800

    update to version 1.1.0, try to reconnect infinitely and log all nats connection events

--- README.md
+++ README.md
@@ -16,12 +16,12 @@ const connectNats = require('nats-event')
 const natsEvent = connectNats('nats://localhost:4222')
   .setEventPrefix('test') // optional
   .setQueueGroup('test') // optional, but you should provide it if you want to launch multiple instances.
-  .on('error', console.error) // optional
     
 natsEvent.listen('hello', msg => console.log(msg))
 
 natsEvent.send('test.hello', 'Bob')
 
+// if you don't use this connection anymore, you can close it.
 natsEvent.close()
 ```
 
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const NATS = require('nats')
 
+const defaultOptions = {
+  url: 'nats://localhost:4222',
+  maxReconnectAttempts: -1, // infinite
+}
+
 module.exports = function connect (options) {
+  // process options
+  if (!options) options = {}
+  if (typeof options === 'string') options = {url: options}
+  options = Object.assign({}, defaultOptions, options)
+
+  // connect nats
   const nats = NATS.connect(options)
+
+  // define default event handlers
+  nats.on('error', (err) => {
+    console.error(err)
+    process.exit(-1)
+  })
+  nats.on('connect', () => console.log('event nats connected'))
+  nats.on('disconnect', () => console.log('event nats disconnected'))
+  nats.on('reconnecting', () => console.log('event nats reconnecting'))
+  nats.on('reconnect', () => console.log('event nats reconnected'))
+  nats.on('close', () => console.log('event nats connection closed'))
 
   let eventPrefix = null
   let queueGroup = null
